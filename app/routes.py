8f90f5c354e1482689dc690c05e5e8ad3e8e08df
Xerozzz <yiterng.19@ichat.sp.edu.sg>
2020-10-04 13:59:15 +0800

    added logout and edited llogin functions

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for,make_response, session
 from app import app, conn
-from app.forms import LoginForm
-from app.backend import addUser, test, loginUser
+from app.forms import LoginForm, RegisterForm
+from app.backend import addUser, test, loginUser, loginRequired, registerUser
 
 # Index Page
 @app.route('/')
 @app.route('/index')
 def index():
+    if loginRequired(session.get('loggedin')) == False:
+        return redirect(url_for('login'))
     return render_template('index.html', title='Home')
 
 # Login Page
@@ -14,9 +16,45 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        username = form.username.data
+        password = form.password.data
+        reply = loginUser(username,password)
+        if reply[0] == True:
+            session['loggedin'] = True
+            session['userid'] = reply[1]
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            flash("Invalid username or password")
+            return redirect(url_for('login'))
     return render_template('login.html', title='Sign In', form=form)
+
+# Logout
+@app.route('/logout')
+def logout():
+    session.pop('loggedin',None)
+    session.pop('userid',None)
+    session.pop('username',None)
+    return render_template('logout.html', title = "Log out")
+
+# Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        reply = registerUser(username,password,email)
+        if reply[0] == True:
+            session['loggedin'] = True
+            session['userid'] = reply[1]
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            flash("Username or email already in use! Try again.")
+            return redirect(url_for('register'))
+    return render_template('register.html', title='Register', form=form)
 
 # Test Page
 @app.route('/testPage')
